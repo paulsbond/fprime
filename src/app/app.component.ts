d29a95ae0be36e7f3d9cf4fd8a5515609b6b5596
Paul Bond <git@paulsbond.co.uk>
2022-10-30 14:55:32 +0000

    Input wavelength or energy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { ElementService } from './element.service';
 
+const hc = 12398.4197386209;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  public wavelength = 0.9795;
+  public energy = Math.round(hc / this.wavelength);
   constructor(public element: ElementService) { }
+
+  wavelengthChanged(wavelength: number) {
+    this.wavelength = wavelength;
+    this.energy = Math.round(hc / wavelength);
+  }
+
+  energyChanged(energy: number) {
+    this.energy = energy;
+    this.wavelength = +(hc / energy).toFixed(4);
+  }
 
   get selected() { return this.element.list.filter(x => x.selected); }
 }
