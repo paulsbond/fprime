01637479c34a042e807e25f61da228bd7013f74a
Paul Bond <git@paulsbond.co.uk>
2022-10-30 15:37:38 +0000

    Selecting new elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ const hc = 12398.4197386209;
 export class AppComponent {
   public wavelength = 0.9795;
   public energy = Math.round(hc / this.wavelength);
+  public search: string = "";
   constructor(public element: ElementService) { }
 
   wavelengthChanged(wavelength: number) {
@@ -21,6 +22,14 @@ export class AppComponent {
   energyChanged(energy: number) {
     this.energy = energy;
     this.wavelength = +(hc / energy).toFixed(4);
+  }
+
+  searchChanged() {
+    const element = this.element.list.find(x => x.label == this.search);
+    if (element) {
+      element.selected = true;
+      this.search = "";
+    }
   }
 
   get selected() { return this.element.list.filter(x => x.selected); }
