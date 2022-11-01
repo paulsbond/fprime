0a2293c6345b2ce05d84aa9d8e9d285a36dd8e04
Paul Bond <git@paulsbond.co.uk>
2022-11-01 10:12:03 +0000

    Emscripen bindings for Gemmi fprime

---
+++ cpp/fprime.cpp
@@ -0,0 +1,43 @@
+#include <emscripten/bind.h>
+#include "fprime.hpp"
+
+using namespace emscripten;
+
+struct Fprime
+{
+    double fp;
+    double fpp;
+};
+
+Fprime fprime(int z, double energy)
+{
+    double fpp[0];
+    double fp = gemmi::cromer_liberman(z, energy, fpp);
+    return {fp, fpp[0]};
+}
+
+std::vector<Fprime> fprimes(int z, std::vector<double> energies)
+{
+    const int npts = energies.size();
+    double *energy = &energies[0];
+    double fp[npts];
+    double fpp[npts];
+    gemmi::cromer_liberman_for_array(z, npts, energy, fp, fpp);
+    std::vector<Fprime> output;
+    for (int i = 0; i < npts; i++)
+    {
+        output.push_back({fp[i], fpp[i]});
+    }
+    return output;
+}
+
+EMSCRIPTEN_BINDINGS(module)
+{
+    value_object<Fprime>("Fprime")
+        .field("fp", &Fprime::fp)
+        .field("fpp", &Fprime::fpp);
+    function("fprime", &fprime);
+    function("fprimes", &fprimes);
+    register_vector<double>("VectorDouble");
+    register_vector<Fprime>("VectorFprime");
+}
