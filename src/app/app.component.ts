86db037b242b950c89c82ba267d768cab8425b35
Paul Bond <git@paulsbond.co.uk>
2022-11-02 10:06:40 +0000

    Angular working with emscripten

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
-import { Element, ElementService } from './element.service';
-import { Chart, ChartConfiguration } from "chart.js";
+import { ApplicationRef, Component, ViewChild } from '@angular/core';
+import { Element, elements } from './elements';
+import { Chart } from "chart.js";
 import annotationPlugin from "chartjs-plugin-annotation";
-import { FprimeService } from './fprime.service';
 import { BaseChartDirective } from 'ng2-charts';
 import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import { ChartConfiguration, ChartDataset } from "chart.js";
 
+declare const Module: any;
 const hc = 12398.4197386209;
 Chart.register(annotationPlugin);
 
@@ -17,44 +18,9 @@ Chart.register(annotationPlugin);
 export class AppComponent {
   public search: string = "";
   public wavelength = 0.9795;
-  private _energy = Math.round(hc / this.wavelength);
-  private energySubject = new Subject<number>();
-  private colors = [
-    "rgb(100, 143, 255)",
-    "rgb(120, 94, 240)",
-    "rgb(220, 38, 127)",
-    "rgb(254, 97, 0)",
-    "rgb(255, 176, 0)",
-  ];
-
-  get energy() { return this._energy; }
-
-  set energy(value: number) {
-    this._energy = value;
-    this.energySubject.next(value);
-  }
-
-  constructor(public element: ElementService, private fprime: FprimeService) {
-    const selenium = this.element.list.find(x => x.symbol == "Se");
-    if (selenium) this.select(selenium);
-    this.energySubject
-      .pipe(debounceTime(500), distinctUntilChanged())
-      .subscribe(this.energyChanged);
-  }
-
-  public chartData: ChartConfiguration<'scatter'>['data'] = {
-    datasets: [
-      {
-        data: this.fprime.f1Data,
-        borderColor: '#648FFF',
-      },
-      {
-        data: this.fprime.f2Data,
-        borderColor: '#648FFF',
-      }
-    ]
-  };
-
+  public elements: Element[] = elements;
+  public selected: Element[] = [];
+  public chartData: ChartConfiguration<'scatter'>['data'] = { datasets: [] };
   public chartOptions: any = {
     animation: { duration: 0 },
     showLine: true,
@@ -77,8 +43,34 @@ export class AppComponent {
     }
   };
   public chartLegend = false;
-
+  private _energy = Math.round(hc / this.wavelength);
+  private energySubject = new Subject<number>();
+  private colors = [
+    "rgb(100, 143, 255)",
+    "rgb(120, 94, 240)",
+    "rgb(220, 38, 127)",
+    "rgb(254, 97, 0)",
+    "rgb(255, 176, 0)",
+  ];
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
+  public get energy() { return this._energy; }
+
+  public set energy(value: number) {
+    this._energy = value;
+    this.energySubject.next(value);
+  }
+
+  constructor(private appRef: ApplicationRef) {
+    Module['onRuntimeInitialized'] = () => {
+      const selenium = elements.find(x => x.symbol == "Se");
+      if (selenium) this.select(selenium);
+      appRef.tick();
+    };
+    this.energySubject
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe(this.energyChanged);
+  }
 
   wavelengthInput() {
     this.energy = Math.round(hc / this.wavelength);
@@ -89,7 +81,7 @@ export class AppComponent {
   }
 
   searchInput() {
-    const element = this.element.list.find(x => x.label == this.search);
+    const element = elements.find(x => x.label == this.search);
     if (element) {
       this.select(element);
       this.search = "";
@@ -105,16 +97,50 @@ export class AppComponent {
     }
   }
 
-  select(element: Element) {
-    element.selected = true;
+  public f1(z: number, energy: number): number {
+    return Module.fprime(z, energy).fp;
+  }
+
+  public f2(z: number, energy: number): number {
+    return Module.fprime(z, energy).fpp;
+  }
+
+  public select(element: Element) {
     element.color = this.colors.shift();
+    this.selected.push(element);
+    this.selected.sort((a, b) => a.z - b.z);
+    this.updateChartData();
   }
 
-  deselect(element: Element) {
+  public deselect(element: Element) {
     if (element.color) this.colors.push(element.color);
-    element.selected = false;
     element.color = undefined;
+    this.selected = this.selected.filter(e => e !== element);
+    this.updateChartData();
   }
 
-  get selected() { return this.element.list.filter(x => x.selected); }
+  private updateChartData() {
+    this.chartData.datasets = [];
+    this.selected.forEach((element) => {
+      this.chartData.datasets = this.chartData.datasets.concat(this.datasets(element));
+    });
+    this.chart?.update();
+  }
+
+  private datasets(element: Element): ChartDataset<'scatter'>[] {
+    const energies = new Module.VectorDouble();
+    for (let energy = 5000; energy <= 20000; energy += 50) {
+      energies.push_back(energy);
+    }
+    const fprimes = Module.fprimes(element.z, energies);
+    const datasets: ChartDataset<'scatter'>[] = [
+      { data: [], borderColor: element.color },
+      { data: [], borderColor: element.color },
+    ];
+    for (let i = 0; i < fprimes.size(); i++) {
+      datasets[0].data.push({ x: energies.get(i), y: fprimes.get(i).fp });
+      datasets[1].data.push({ x: energies.get(i), y: fprimes.get(i).fpp });
+    }
+    return datasets;
+  }
 }
